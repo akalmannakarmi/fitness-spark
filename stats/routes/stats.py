34d38c8baa68dae14719589fe4d093a22d5140b1
akalmannakarmi <akalnakarmi1@gmail.com>
2025-11-07 19:18:48 +0545

    fix url and compose file

--- stats/routes/stats.py
+++ stats/routes/stats.py
@@ -1,24 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Request
-from ..schemas import ModelsOut,ModelOut
-from ..crud import db_get_models,db_get_model
-from ..wraper import update_stats,Models,Actions
-from auth.wraper import admin_user,User
-from bson import ObjectId
+from fastapi import APIRouter, Depends, HTTPException
+from ..schemas import ModelsOut, ModelOut
+from ..crud import db_get_models, db_get_model
+from ..wraper import update_stats, Models, Actions
+from auth.wraper import admin_user
 
 router = APIRouter()
 
 
-@router.get("/models/", response_model=ModelsOut)
-@update_stats(Models.Stats,Actions.Read)
-async def get_models(user:User=Depends(admin_user)):
+@router.get("/models", response_model=ModelsOut)
+@update_stats(Models.Stats, Actions.Read)
+async def get_models(_=Depends(admin_user)):
     models = await db_get_models()
-    return {"models":models}
+    return {"models": models}
 
 
 @router.get("/model/{model_id}", response_model=ModelOut)
-@update_stats(Models.Stats,Actions.Read)
-async def get_model(model_id:str,user:User=Depends(admin_user)):
+@update_stats(Models.Stats, Actions.Read)
+async def get_model(model_id: str, _=Depends(admin_user)):
     model = await db_get_model(model_id)
     if model is None:
-        raise HTTPException(status_code=404,detail="Model Not Found")
+        raise HTTPException(status_code=404, detail="Model Not Found")
     return model
